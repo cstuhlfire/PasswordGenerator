2a5ddd6d47c14c5dd66fcb525357d25db766e87f
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-03-18 02:38:19 -0500

    add/ Added character set arrays and built selection array based on user choices

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,13 @@
 // Assignment Code
+// Define object to store user selections
+let userChoices = {
+  length: 0,
+  lowerCase: false,
+  upperCase: false,
+  numbers: false,
+  special: false
+};
+
 var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
@@ -18,42 +27,82 @@ function writePassword() {
 // Returns pw (password)
 function generatePassword() {
   let pw = ""; //string for building password
-  
-  let len = promptPasswordLength();
+
+  resetChoices();
+  promptPasswordLength();
   
   // If the user did not cancel, prompt for the other criteria
-  if (len !== null){
-    let userChoices = getChoices();
-    console.log(userChoices);
+  if (userChoices.length !== null){
+    getChoices();
+    pw = buildPassword();
+
+    return pw;
   }
-  
-  return pw;
+  else {
+    return pw;
+  }
+}
+
+//************************************************************************ */
+// Funtion - Reset userChoices object
+function resetChoices(){
+  userChoices = {
+    length: 0,
+    lowerCase: false,
+    upperCase: false,
+    numbers: false,
+    special: false
+  };
+}
+
+//************************************************************************ */
+// Funtion - Build password based on  userChoices
+function buildPassword(){
+  let lowerArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let upperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  let numberArray = ["0","1","2","3","4","5","6","7","8","9"];
+  let specialArray = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+  let selectionArray = [];
+
+  // Build selection array based on userChoices
+  if (userChoices.lowerCase) {
+    selectionArray = selectionArray.concat(lowerArray);
+  }
+  if(userChoices.upperCase) {
+    selectionArray = selectionArray.concat(upperArray);
+  }
+  if(userChoices.numbers) {
+    selectionArray = selectionArray.concat(numberArray);
+  }
+  if(userChoices.special) {
+    selectionArray = selectionArray.concat(specialArray);
+  }
+
+  console.log(selectionArray);
+
+  return "password";
 }
 
 //************************************************************************ */
 // Funtion - Prompt for password length and check if valid number
-// Returns len (length)
 function promptPasswordLength() {
-  let len = 0;
   
   //prompt user for number until input criteria is met
-  while (isNaN(len) || (len < 8 || len > 128)){
-    len = prompt("Enter the length of the password.\n\nMust be a number between 8 and 128.");
+  while (isNaN(userChoices.length) || (userChoices.length < 8 || userChoices.length > 128)){
+    userChoices.length = prompt("Enter the length of the password.\n\nMust be a number between 8 and 128.");
     
-    // if the user cancels, return len
-    if (len === null){
-      return len;
+    // if the user cancels, return
+    if (userChoices.length === null){
+      return;
     }
   }
-  // return len for length of password desired
-  return len;
+  return;
 }
 
 //************************************************************************ */
 // Function - Gets user choices for password generation and verifies a selection is made.
-// Returns userChoices object
 function getChoices () {
-  let userChoices = promptCharTypes();
+  promptCharTypes();
 
   // If no characters are chosen prompt user to choose or cancel
   if (userChoices.lowerCase &&
@@ -61,7 +110,7 @@ function getChoices () {
     userChoices.numbers &&
     userChoices.special) {
       // User has chosen at least one character set
-      return userChoices;
+      return;
   }
   else {
     let letsContinue = true;
@@ -77,11 +126,11 @@ function getChoices () {
         
         if (letsContinue) {
           // User has chosen to continue generating a password
-          userChoices = promptCharTypes();
-          return userChoices;
+          promptCharTypes();
+          return;
         }
         else {
-          return userChoices;
+          return;
         }
     }
   }
@@ -90,15 +139,7 @@ function getChoices () {
 
 //************************************************************************ */
 // Function - Prompt user for character preferences
-// Returns userChoices object
 function promptCharTypes() {
-  let userChoices = {
-    lowerCase: false,
-    upperCase: false,
-    numbers: false,
-    special: false
-  };
-
   // Allow lowercase?
   userChoices.lowerCase = confirm("Do you want to include lowercase letters in your password?\n\nChoose OK to include and Cancel to exclude.");
 
@@ -111,7 +152,7 @@ function promptCharTypes() {
   // Allow special characters?
   userChoices.special = confirm("Do you want to include special characters in your password?\n\nChoose OK to include and Cancel to exclude.");
 
-  return userChoices;
+  return;
 }
 
 // query length and check if valid ***
@@ -119,9 +160,9 @@ function promptCharTypes() {
 // query uppercase ***
 // query numeric ***
 // query special characters ***
-// verify selection criteria (validate at least one criteria is chosen)
-// continue prompting until selection criteria satisfied
-// based on selection, build selection array
+// verify selection criteria (validate at least one criteria is chosen) ***
+// continue prompting until selection criteria satisfied ***
+// based on selection, build selection object ***
 // for the length of the password, randomly select from the selection array and build the password string
 // write password to alert
 // write password to screen
