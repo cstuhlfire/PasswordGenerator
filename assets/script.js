4b1968a548b7877b074e074aea45fa10f83cb9f4
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-03-18 03:15:13 -0500

    bug fixes/ Corrected verification and undefined password when user does not make a choice

--- assets/script.js
+++ assets/script.js
@@ -20,6 +20,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  return;
 }
 
 //************************************************************************ */
@@ -33,14 +35,12 @@ function generatePassword() {
   
   // If the user did not cancel, prompt for the other criteria
   if (userChoices.length !== null){
-    getChoices();
-    pw = buildPassword();
-
-    return pw;
+    let letsContinue = getChoices();
+    if (letsContinue) {
+      pw = buildPassword();
+    }
   }
-  else {
-    return pw;
-  }
+  return pw;
 }
 
 //************************************************************************ */
@@ -62,7 +62,7 @@ function buildPassword(){
   let upperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   let numberArray = ["0","1","2","3","4","5","6","7","8","9"];
   let specialArray = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
-  
+
   let selectionArray = [];
   let password = "";
 
@@ -107,7 +107,9 @@ function promptPasswordLength() {
 //************************************************************************ */
 // Function - Gets user choices for password generation and verifies a selection is made.
 function getChoices () {
+  let letsContinue = true;
   promptCharTypes();
+
 
   // If no characters are chosen prompt user to choose or cancel
   if (userChoices.lowerCase &&
@@ -118,7 +120,7 @@ function getChoices () {
       return;
   }
   else {
-    let letsContinue = true;
+    letsContinue = true;
     
     // User has not selected a character set
     while (letsContinue && 
@@ -132,13 +134,13 @@ function getChoices () {
         if (letsContinue) {
           // User has chosen to continue generating a password
           promptCharTypes();
-          return;
         }
         else {
-          return;
+          return letsContinue;
         }
+      }
     }
-  }
+    return letsContinue;
 }
   
 
@@ -168,6 +170,6 @@ function promptCharTypes() {
 // verify selection criteria (validate at least one criteria is chosen) ***
 // continue prompting until selection criteria satisfied ***
 // based on selection, build selection object ***
-// for the length of the password, randomly select from the selection array and build the password string
-// write password to alert
-// write password to screen
+// for the length of the password, randomly select from the selection array and build the password string ***
+// write password to alert ***
+// write password to screen ***
